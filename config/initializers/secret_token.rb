51745c3282d4bf71087a02116df9b3cf2ef0f26a
Amber <amber@lexblog.com>
2011-11-23 14:24:05 -0800

    First commit. Started RoR app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TweetCalendar::Application.config.secret_token = '1d2a0c901e21574ce35153e88a55382942b60d01bf5c706011b061e588560a32d71ee9aa9f0d83cceac68fcab7ec215a4bc90645f01b2703cdf19d7cfabbbd30'
